d01af3d1b3432735e92139d05a2b3e11545dd248
Asmita <asmita2018fzd@gmail.com>
2025-04-01 12:11:30 +0530

    feat: added templates for views

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'recipe')
+    readonly_fields = ('recipe', 'name', 'quantity')  # Users cannot edit these
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_superuser  # Only admin can change
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient, IngredientAdmin)
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    
+    path('', views.display_recipes, name='home'),
+    path('create_recipe/', views.create_recipe, name='create_recipe'),
+    path('display_recipes/', views.display_recipes, name='display_recipes'),    
+    path('recipe_detail/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
+    path('update_recipe/<int:recipe_id>/', views.update_recipe, name='update_recipe'),
+    path('delete_recipe/<int:recipe_id>/', views.delete_recipe, name='delete_recipe'),    
 ]
