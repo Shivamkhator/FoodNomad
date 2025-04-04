a0f8e29bfce4cb8e70065c3040ec8170dc84f1ef
Shivam Khator <shivamkhator2005@gmail.com>
2025-04-04 23:45:33 +0530

    Fix: Error due to wrong Base_URl

--- recommendations/urls.py
+++ recommendations/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('stores/',views.stores, name='stores'),
+    path('grocery_stores/', views.grocery_stores_view, name='grocery_stores'),
     
 ]
